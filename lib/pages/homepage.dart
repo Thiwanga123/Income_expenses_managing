e203f35c7ec18944aaff4934fb9d9926b5a08c69
Thiwanga123 <info.thiwangasri@gmail.om>
2024-10-14 21:55:15 +0530

    added new modifications

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,24 +12,58 @@ class Home extends StatelessWidget {
         child: Stack(
           children: [
             Container(
-              color: Color(0xff2A7C76),
               height: 287,
               width: double.infinity,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Good afternoon,' , style: TextStyle(color: Colors.white, fontSize: 14)),
-                      Text( 'Thiwanga' , style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                  Icon( Icons.menu, color: Colors.white),
-                ],
+              decoration: const BoxDecoration(
+                color: Color(0xff2A7C76),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
               ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top:35,
+                    left: 340,
+                  child:ClipRRect(
+                    borderRadius: BorderRadius.circular(7),
+                    child:Container(
+                      height: 40,
+                      width: 40,
+                      color:const Color.fromRGBO(250, 250, 250, 0.1),
+                      child:  const Icon(Icons.notification_add_outlined, color: Colors.white,),
+                    )
+                  ),
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsets.only(top:35, left: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Good afternoon", 
+                        style: 
+                        TextStyle(
+                          color: Colors.white, 
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14),
+                          ),
+                        Text("Thiwanga", 
+                        style:
+                        TextStyle(
+                          color: Colors.white, 
+                          fontWeight: FontWeight.w600, 
+                          fontSize: 20),
+                          ),
+                      ],
+                    ),
+                  )
+                  
+                ],
+              )
             ),
+        
           ]
           ,
         ),
