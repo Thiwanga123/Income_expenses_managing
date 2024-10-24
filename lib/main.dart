c30da1d9ecbff588c63f56cd25cffe26be7148e1
Thiwanga123 <info.thiwangasri@gmail.om>
2024-10-24 09:42:43 +0530

    completed bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:income_expenses_managing/widget/bottomnavigation.dart';
 import 'pages/homepage.dart';
 
 void main() async{
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Bottom(),
     );
   }
 }
