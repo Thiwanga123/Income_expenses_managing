e8728ead42587f1d831867cdbc79b1e184817589
Thiwanga123 <info.thiwangasri@gmail.om>
2024-10-24 09:56:53 +0530

    started add expenses page

---
+++ lib/pages/add_expenses.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddExpenses extends StatefulWidget {
+  const AddExpenses({super.key});
+
+  @override
+  State<AddExpenses> createState() => _AddExpensesState();
+}
+
+class _AddExpensesState extends State<AddExpenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
