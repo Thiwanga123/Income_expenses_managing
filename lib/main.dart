db0e9c06e591db0d3e6a49fc487e0024497f719b
Thiwanga123 <info.thiwangasri@gmail.om>
2024-10-24 15:17:00 +0530

    updated with the changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:income_expenses_managing/widget/bottomnavigation.dart';
-import 'pages/homepage.dart';
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
